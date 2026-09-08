fa62c368d61b0a1c84403a18d872dac97d5bda81
jineey030 <92289949+jineey030@users.noreply.github.com>
2026-09-08 15:06:48 +0900

    Refactor: Project type to interface and add property
    Changed Project type to an interface and added technicalHighlights property.

--- frontend/src/pages/introduce/types/introduce.ts
+++ frontend/src/pages/introduce/types/introduce.ts
@@ -1,16 +1,18 @@
-export type Project = {
+export interface Project {
   id: string;
   title: string;
   description: string;
   stack: string[];
-
   features: string[];
   role: string;
   challenges: string[];
-
+  technicalHighlights: {
+    title: string;
+    description: string;
+  }[];
   github?: string;
   demo?: string;
-};
+}
 
 export interface SkillGroup {
   category: string;
