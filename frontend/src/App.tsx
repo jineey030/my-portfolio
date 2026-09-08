8eaefbf04f67d7b3593bca58e1ebcbb0a1a3d76a
jineey030 <92289949+jineey030@users.noreply.github.com>
2026-09-08 14:10:31 +0900

    Refactor: Add ScrollToHash component and update comments
    Refactor App component to include ScrollToHash and improve comments.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,44 @@
 import { Routes, Route, BrowserRouter } from 'react-router';
 import './App.css';
 
-// import page
+// pages
 import Introduce from './pages/introduce/pages';
 import ProjectDetail from './pages/project/ProjectDetail';
+
+// components
+import ScrollToHash from './pages/project/components/ScrollToHash';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Introduce />} />
+      {/* hash 기반 스크롤 처리 */}
+      <ScrollToHash />
 
+      <Routes>
+        {/* 메인 페이지 */}
+        <Route
+          path="/"
+          element={<Introduce />}
+        />
+
+        {/* 프로젝트 상세 페이지 */}
         <Route
           path="/projects/:projectId"
           element={<ProjectDetail />}
         />
 
+        {/* 존재하지 않는 페이지 */}
         <Route
           path="*"
-          element={<div>페이지를 찾을 수 없습니다.</div>}
+          element={
+            <div>
+              페이지를 찾을 수 없습니다.
+            </div>
+          }
         />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
