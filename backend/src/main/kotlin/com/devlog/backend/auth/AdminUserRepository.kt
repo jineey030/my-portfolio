1f63a2b7bced0a1abc47e5406cbe2e2b3613db63
jineey030 <92289949+jineey030@users.noreply.github.com>
2026-09-10 16:35:36 +0900

    Delete backend/src/main/kotlin/com/devlog/backend/auth directory

--- backend/src/main/kotlin/com/devlog/backend/auth/AdminUserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.devlog.backend.auth
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AdminUserRepository : JpaRepository<AdminUser, Long> {
-
-    fun findByUsername(username: String): AdminUser?
-}
