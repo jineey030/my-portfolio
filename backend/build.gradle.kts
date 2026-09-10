851984456417e3e465d5044dd8faa2b2e3b00a91
jineey030 <92289949+jineey030@users.noreply.github.com>
2026-09-10 16:10:46 +0900

    Add JSON Web Token dependencies to build.gradle.kts

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -30,6 +30,10 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter-security")
 
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6") 
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6") 
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
